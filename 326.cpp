3f47c3c7bed02ad33c4aa302d57733bc94c2dd69
henilgamit <gamithenil384@gmail.com>
2025-08-05 22:23:43 +0530

    Create 326.cpp
    5 aug

---
+++ 326.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n < 1) return false;
+        for(int i=0; pow(3,i)<=n; i++){
+            if(pow(3,i) == n) return true;
+        }
+        return false;
+    }
+};
